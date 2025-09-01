b1744dc065279e47484e9bc1980ea291a6fc89d6
SanjarKhamraev <ghalk585@gmail.com>
2025-09-01 11:20:53 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
-import './globals.css';
+import './globals.css'
 
-export const metadata = {
-  title: 'POSEIDON JEWELRY',
-  description: 'Online jewelry boutique',
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
